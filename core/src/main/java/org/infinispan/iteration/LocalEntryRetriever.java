32544c1a29235ecff702b35fcffa12ced2959a76
William Burns <mudokonman@gmail.com>
2014-08-12 11:37:52 -0400

    BZ1128811 ISPN-4602 Verify EntryIterator works with MarshalledValues * Added unwrapping of MarshalledValues * Changed MarshalledValue to use inner object hash code

--- core/src/main/java/org/infinispan/iteration/LocalEntryRetriever.java
+++ core/src/main/java/org/infinispan/iteration/LocalEntryRetriever.java
@@ -22,6 +22,7 @@ import org.infinispan.filter.KeyValueFilter;
 import org.infinispan.filter.KeyValueFilterAsKeyFilter;
 import org.infinispan.marshall.core.MarshalledEntry;
 import org.infinispan.filter.Converter;
+import org.infinispan.marshall.core.MarshalledValue;
 import org.infinispan.notifications.Listener;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryActivated;
 import org.infinispan.notifications.cachelistener.event.CacheEntryActivatedEvent;
@@ -192,14 +193,16 @@ public class LocalEntryRetriever<K, V> implements EntryRetriever<K, V> {
                try {
                   int interruptCheck = 0;
                   for (InternalCacheEntry entry : dataContainer) {
-                     K key = (K) entry.getKey();
+                     InternalCacheEntry clone = entryFactory.create(unwrapMarshalledvalue(entry.getKey()),
+                                                                    unwrapMarshalledvalue(entry.getValue()), entry);
+                     K key = (K) clone.getKey();
                      if (filter != null) {
-                        if (!filter.accept(key, (V) entry.getValue(), entry.getMetadata())) {
+                        if (!filter.accept(key, (V) clone.getValue(), entry.getMetadata())) {
                            continue;
                         }
                      }
 
-                     action.apply(key, entry);
+                     action.apply(key, clone);
                      if (interruptCheck++ % batchSize == 0) {
                         if (Thread.interrupted()) {
                            throw new CacheException("Entry Iterator was interrupted!");
@@ -427,5 +430,12 @@ public class LocalEntryRetriever<K, V> implements EntryRetriever<K, V> {
             nextLock.unlock();
          }
       }
+   }
+
+   protected static <T> T unwrapMarshalledvalue(T value) {
+      if (value instanceof MarshalledValue) {
+         return (T) ((MarshalledValue) value).get();
+      }
+      return value;
    }
 }
