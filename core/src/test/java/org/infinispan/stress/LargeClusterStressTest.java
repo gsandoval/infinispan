3ba3bc4948b8324524d781899a244e7402b004a3
Dan Berindei <dan@infinispan.org>
2015-01-12 17:47:48 +0200

    testing

--- core/src/test/java/org/infinispan/stress/LargeClusterStressTest.java
+++ core/src/test/java/org/infinispan/stress/LargeClusterStressTest.java
@@ -13,6 +13,10 @@ import org.infinispan.test.fwk.TestResourceTracker;
 import org.infinispan.topology.LocalTopologyManager;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Test that we're able to start a large cluster in a single JVM.
  *
@@ -31,36 +35,56 @@ public class LargeClusterStressTest extends MultipleCacheManagersTest {
    }
 
    public void testLargeCluster() throws Exception {
-      Configuration distConfig = getDefaultClusteredCacheConfig(CacheMode.DIST_SYNC, false).clustering().stateTransfer().awaitInitialTransfer(false).build();
-      Configuration replConfig = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, false).clustering().stateTransfer().awaitInitialTransfer(false).build();
+      final Configuration distConfig = getDefaultClusteredCacheConfig(CacheMode.DIST_SYNC, false).clustering().stateTransfer().awaitInitialTransfer(false).build();
+      final Configuration replConfig = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, false).clustering().stateTransfer().awaitInitialTransfer(false).build();
+
+      // Start the caches (and the JGroups channels) in separate threads
+      Future<Object>[] futures = new Future[NUM_NODES];
       for (int i = 0; i < NUM_NODES; i++) {
-         GlobalConfigurationBuilder gcb = new GlobalConfigurationBuilder();
-         gcb.globalJmxStatistics().allowDuplicateDomains(true);
-         gcb.transport().defaultTransport().nodeName(TestResourceTracker.getNameForIndex(i));
-         BlockingThreadPoolExecutorFactory remoteExecutorFactory = new BlockingThreadPoolExecutorFactory(
-               10, 1, 0, 60000);
-         gcb.transport().remoteCommandThreadPool().threadPoolFactory(remoteExecutorFactory);
-         EmbeddedCacheManager cm = new DefaultCacheManager(gcb.build());
-         registerCacheManager(cm);
-         for (int j = 0; j < NUM_CACHES; j++) {
-            if (j % 2 == 0) {
-               cm.defineConfiguration("replcache" + j, replConfig);
-               Cache<Object, Object> cache = cm.getCache("replcache" + j);
-               cache.put(cm.getAddress(), "bla");
-            } else {
-               cm.defineConfiguration("distcache" + j, distConfig);
-               Cache<Object, Object> cache = cm.getCache("distcache" + j);
-               cache.put(cm.getAddress(), "bla");
+         final String nodeName = TestResourceTracker.getNameForIndex(i);
+         futures[i] = fork(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+               GlobalConfigurationBuilder gcb = new GlobalConfigurationBuilder();
+               gcb.globalJmxStatistics().allowDuplicateDomains(true);
+               gcb.transport().defaultTransport().nodeName(nodeName);
+               BlockingThreadPoolExecutorFactory remoteExecutorFactory = new BlockingThreadPoolExecutorFactory(
+                     10, 1, 0, 60000);
+               gcb.transport().remoteCommandThreadPool().threadPoolFactory(remoteExecutorFactory);
+               EmbeddedCacheManager cm = new DefaultCacheManager(gcb.build());
+               registerCacheManager(cm);
+               for (int j = 0; j < NUM_CACHES; j++) {
+                  if (j % 2 == 0) {
+                     cm.defineConfiguration("repl-cache-" + j, replConfig);
+                  } else {
+                     cm.defineConfiguration("dist-cache-" + j, distConfig);
+                  }
+               }
+               for (int j = 0; j < NUM_CACHES; j++) {
+                  if (j % 2 == 0) {
+                     Cache<Object, Object> cache = cm.getCache("repl-cache-" + j);
+                     cache.put(cm.getAddress(), "bla");
+                  } else {
+                     Cache<Object, Object> cache = cm.getCache("dist-cache-" + j);
+                     cache.put(cm.getAddress(), "bla");
+                  }
+               }
+               log.infof("Started cache manager %s", cm.getAddress());
+               return null;
             }
-         }
-         log.infof("Started cache manager %s", cm.getAddress());
-         // TODO Test is unstable without this wait, needs more investigation after JGRP-1899 is fixed
-         TestingUtil.blockForMemberToFail(30000, cacheManagers.toArray(new EmbeddedCacheManager[0]));
+         });
+      }
+
+      for (int i = 0; i < NUM_NODES; i++) {
+         futures[i].get(30, TimeUnit.SECONDS);
       }
 
       for (int j = 0; j < NUM_CACHES; j++) {
-         waitForClusterToForm("replcache" + j);
-         waitForClusterToForm("distcache" + j);
+         if (j % 2 == 0) {
+            waitForClusterToForm("repl-cache-" + j);
+         } else {
+            waitForClusterToForm("dist-cache-" + j);
+         }
       }
       TestingUtil.extractGlobalComponent(manager(0), LocalTopologyManager.class).setRebalancingEnabled(false);
    }
